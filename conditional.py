aef3ca678425de79268cea11c2f577f2021e2176
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-03-13 09:46:48 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter your marks : "))
+
+if(marks >= 90):
+    grade = "A"
+elif(marks < 90 and marks >= 80):
+    grade = "B"
+elif(marks < 80 and marks >= 70):
+    grade = "C"
+else:
+    grade = "D"    
+    
+print("Grade of the Student : ",grade)
