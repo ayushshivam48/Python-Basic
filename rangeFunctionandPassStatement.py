ae97ff81c7b3690c335a145e5b77434faea9e2f9
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-03-15 11:02:42 +0530

    Add files via upload

---
+++ rangeFunctionandPassStatement.py
@@ -0,0 +1,16 @@
+for i in range(10): #range(stop)
+    print(i)
+    
+for i in range(2, 10):   #range(start, stop)
+    print(i)
+
+for i in range(2, 10, 2):   #range(start, stop, step size)
+    print(i)
+    
+
+# note :- Step size is either +ve or -ve
+
+
+for i in range(5):
+    pass
+print("skip the work for further use")
