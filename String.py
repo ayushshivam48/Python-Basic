f795efc8b6a6cb5d9fc0118ab8d1175d4c4d83cc
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2025-03-15 11:03:23 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,10 @@
+name = input("enter the string : ")
+print("Length of the String : ",len(name))
+print("Occurance of 's' : ",name.count("s"))
+print(name.endswith("am"))
+print(name.capitalize())
+print(name.replace("a", "pi"))
+print(name.find("ayush"))
+print(name[0:6])
+print(name[7:])   #7 to last
+print(name[:6])   #1 to 5th
